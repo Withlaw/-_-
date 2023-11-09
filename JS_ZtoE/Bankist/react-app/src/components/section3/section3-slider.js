58829e195cd2de184d361ee823c6fbea355562d3
Withlaw <sanxang@gmail.com>
2023-11-10 01:13:13 +0900

    feat: slider keydown 추가

--- JS_ZtoE/Bankist/react-app/src/components/section3/section3-slider.js
+++ JS_ZtoE/Bankist/react-app/src/components/section3/section3-slider.js
@@ -1,74 +1,99 @@
+import { useEffect, useState } from "react";
 import classes from "./section3-slider.module.css";
+import user1 from "../../assets/img/user-1.jpg";
+import user2 from "../../assets/img/user-2.jpg";
+import user3 from "../../assets/img/user-3.jpg";
+
 function Section3Slider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const handleBtnR = e => {
+    if (currentSlide < 2) setCurrentSlide(prev => ++prev);
+    else setCurrentSlide(0);
+  };
+  const handleBtnL = e => {
+    if (currentSlide > 0) setCurrentSlide(prev => --prev);
+    else setCurrentSlide(2);
+  };
+  const handleBtnDots = (e, i) => {
+    setCurrentSlide(i);
+  };
+  const handleKeydown = e => {
+    e.key === "ArrowLeft" && handleBtnL();
+    e.key === "ArrowRight" && handleBtnR();
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeydown);
+    return () => document.removeEventListener("keydown", handleKeydown);
+  }, [currentSlide]);
   return (
     <div className={classes.slider}>
-      <div className={classes.slide}>
-        <div className={classes.testimonial}>
-          <h5 className={classes.header}>Best financial decision ever!</h5>
-          <blockquote className={classes.text}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            Accusantium quas quisquam non? Quas voluptate nulla minima deleniti
-            optio ullam nesciunt, numquam corporis et asperiores laboriosam
-            sunt, praesentium suscipit blanditiis. Necessitatibus id alias
-            reiciendis, perferendis facere pariatur dolore veniam autem esse non
-            voluptatem saepe provident nihil molestiae.
-          </blockquote>
-          <address className={classes.author}>
-            <img src="img/user-1.jpg" alt="" className={classes.photo} />
-            <h6 className={classes.name}>Aarav Lynn</h6>
-            <p className={classes.location}>San Francisco, USA</p>
-          </address>
+      {slideContent.map((el, idx) => (
+        <div
+          className={classes.slide}
+          style={{ transform: `translateX(${(idx - currentSlide) * 100}%)` }}
+        >
+          <div className={classes.testimonial}>
+            <h5 className={classes.header}>{el.header}</h5>
+            <blockquote className={classes.text}>{el.text}</blockquote>
+            <address className={classes.author}>
+              <img src={el.imgSrc} alt="" className={classes.photo} />
+              <h6 className={classes.name}>{el.name}</h6>
+              <p className={classes.location}>{el.location}</p>
+            </address>
+          </div>
         </div>
-      </div>
+      ))}
 
-      <div className={classes.slide}>
-        <div className={classes.testimonial}>
-          <h5 className={classes.header}>
-            The last step to becoming a complete minimalist
-          </h5>
-          <blockquote className={classes.text}>
-            Quisquam itaque deserunt ullam, quia ea repellendus provident,
-            ducimus neque ipsam modi voluptatibus doloremque, corrupti laborum.
-            Incidunt numquam perferendis veritatis neque repellendus. Lorem,
-            ipsum dolor sit amet consectetur adipisicing elit. Illo deserunt
-            exercitationem deleniti.
-          </blockquote>
-          <address className={classes.author}>
-            <img src="img/user-2.jpg" alt="" className={classes.photo} />
-            <h6 className={classes.name}>Miyah Miles</h6>
-            <p className={classes.location}>London, UK</p>
-          </address>
-        </div>
-      </div>
-
-      <div className={classes.slide}>
-        <div className={classes.testimonial}>
-          <h5 className={classes.header}>Finally free from old-school banks</h5>
-          <blockquote className={classes.text}>
-            Debitis, nihil sit minus suscipit magni aperiam vel tenetur incidunt
-            commodi architecto numquam omnis nulla autem, necessitatibus
-            blanditiis modi similique quidem. Odio aliquam culpa dicta beatae
-            quod maiores ipsa minus consequatur error sunt, deleniti saepe
-            aliquid quos inventore sequi. Necessitatibus id alias reiciendis,
-            perferendis facere.
-          </blockquote>
-          <address className={classes.author}>
-            <img src="img/user-3.jpg" alt="" className={classes.photo} />
-            <h6 className={classes.name}>Francisco Gomes</h6>
-            <p className={classes.location}>Lisbon, Portugal</p>
-          </address>
-        </div>
-      </div>
-
-      <button className={`${classes.btn} ${classes["btn-left"]}`}>
+      <button
+        className={`${classes.btn} ${classes["btn-left"]}`}
+        onClick={handleBtnL}
+      >
         &larr;
       </button>
-      <button className={`${classes.btn} ${classes["btn-right"]}`}>
+      <button
+        className={`${classes.btn} ${classes["btn-right"]}`}
+        onClick={handleBtnR}
+      >
         &rarr;
       </button>
-      <div className={classes.dots}></div>
+      <div className={classes.dots}>
+        {slideContent.map((_, idx) => (
+          <button
+            className={`${classes.dot} ${
+              currentSlide === idx ? classes["dot-active"] : ""
+            }`}
+            onClick={e => {
+              handleBtnDots(e, idx);
+            }}
+          ></button>
+        ))}
+      </div>
     </div>
   );
 }
+const slideContent = [
+  {
+    header: "Best financial decision ever!",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium quas quisquam non? Quas voluptate nulla minima delenitioptio ullam nesciunt, numquam corporis et asperiores laboriosamsunt, praesentium suscipit blanditiis. Necessitatibus id alias reiciendis, perferendis facere pariatur dolore veniam autem esse nonvoluptatem saepe provident nihil molestiae.",
+    imgSrc: user1,
+    name: "Aarav Lynn",
+    location: "San Francisco, USA",
+  },
+  {
+    header: "The last step to becoming a complete minimalist",
+    text: "Quisquam itaque deserunt ullam, quia ea repellendus provident, ducimus neque ipsam modi voluptatibus doloremque, corrupti laborum. Incidunt numquam perferendis veritatis neque repellendus. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo deserunt exercitationem deleniti.",
+    imgSrc: user2,
+    name: "Miyah Miles",
+    location: "London, UK",
+  },
+  {
+    header: "Finally free from old-school banks",
+    text: "Debitis, nihil sit minus suscipit magni aperiam vel tenetur incidunt commodi architecto numquam omnis nulla autem, necessitatibus blanditiis modi similique quidem. Odio aliquam culpa dicta beatae quod maiores ipsa minus consequatur error sunt, deleniti saepe aliquid quos inventore sequi. Necessitatibus id alias reiciendis, perferendis facere.",
+    imgSrc: user3,
+    name: "Francisco Gomes",
+    location: "Lisbon, Portugal",
+  },
+];
 
 export default Section3Slider;
