43b3c793c0192b0cb6b77a21e06e1d7155001a23
Withlaw <sanxang@gmail.com>
2023-11-09 23:05:11 +0900

    feat: section4 reveal

--- JS_ZtoE/Bankist/react-app/src/section4.js
+++ JS_ZtoE/Bankist/react-app/src/section4.js
@@ -1,14 +1,32 @@
 import { useState } from "react";
 import Modal from "./components/modal/modal";
+import useObserve from "./hooks/useObserve";
 
 function Section4() {
   const [isModal, setIsModal] = useState(false);
+  const [isReveal, setIsReveal] = useState(false);
+  const { observerTarget } = useObserve(
+    (entry, observer) => {
+      if (!entry.isIntersecting) return;
+      setIsReveal(true);
+      observer.unobserve(entry.target);
+    },
+    {
+      root: null,
+      threshold: 0.2,
+    }
+  );
   const handleClickModal = e => {
     e.preventDefault();
     setIsModal(prev => !prev);
   };
   return (
-    <section className="section section--sign-up">
+    <section
+      className={`section section--sign-up ${
+        isReveal ? "" : "section--hidden"
+      }`}
+      ref={observerTarget}
+    >
       <div className="section__title">
         <h3 className="section__header">
           The best day to join Bankist was one year ago. The second best is
