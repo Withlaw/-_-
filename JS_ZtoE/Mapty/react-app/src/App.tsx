9ee41ecdbf2a4934295da03bbe10b15ed32e50a9
Withlaw <sanxang@gmail.com>
2023-11-12 19:11:38 +0900

    mapty/design: loading spinner

--- JS_ZtoE/Mapty/react-app/src/App.tsx
+++ JS_ZtoE/Mapty/react-app/src/App.tsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 // import logo from "./logo.svg";
-import "./globals.css";
-import Workouts from "./components/workouts";
-import Copyright from "./components/footer/Copyright";
-import Map from "./components/map";
+import './globals.css';
+import Workouts from './components/workouts';
+import Copyright from './components/footer/Copyright';
+import Map from './components/map';
 
-import logo from "./assets/logo.png";
+import logo from './assets/logo.png';
+import LoadingSpinner from './components/layout/LoadingSpinner';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <>
       <div className="sidebar">
         <img className="logo" src={logo} alt="Logo" />
-        <Workouts />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <>
+            <Workouts />
+          </>
+        )}
         <Copyright />
       </div>
       <Map />
---
+++ JS_ZtoE/Mapty/react-app/src/components/layout/LoadingSpinner.js
@@ -0,0 +1,10 @@
+const LoadingSpinner = () => {
+  return (
+    <>
+      <div className="spinner"></div>
+      <div className="spinner--backdrop "></div>
+    </>
+  );
+};
+
+export default LoadingSpinner;
