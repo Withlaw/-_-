5294f63db0b0662e73bc8afeb8d43f29bb38cc0f
Withlaw <sanxang@gmail.com>
2023-11-14 22:49:41 +0900

    mapty: workout class 수정

--- JS_ZtoE/Mapty/react-app/src/state.tsx
+++ JS_ZtoE/Mapty/react-app/src/state.tsx
@@ -1,42 +1,23 @@
-interface WorkoutDataProps {
-  type: 'Running' | 'Cycling';
+// Workout
+interface WorkoutProps {
   id: number;
   distance: number;
   duration: number;
   position: [number, number];
   date: string;
-  cadence?: number;
-  elevationGain?: number;
 }
 
-export class WorkoutData {
-  type: 'Running' | 'Cycling';
+class Workout {
   id: string;
   date: string;
   distance: number;
   duration: number;
   position: [number, number];
-  // Running
-  cadence?: number;
-  pace?: number;
-  // Cycling
-  elevationGain?: number;
-  speed?: number;
 
-  constructor(values: WorkoutDataProps) {
-    this.type = values.type;
+  constructor(values: WorkoutProps) {
     this.distance = values.distance; // in km
     this.duration = values.duration; // in min
     this.position = values.position; // [lat, lng]
-
-    if (this.type === 'Running') {
-      this.cadence = values.cadence;
-      this.pace = this._calcPace();
-    }
-    if (this.type === 'Cycling') {
-      this.elevationGain = values.elevationGain;
-      this.speed = this._calcSpeed();
-    }
 
     this.id = Date.now().toString().slice(-10);
     this.date = this._setDescription({
@@ -55,16 +36,50 @@ export class WorkoutData {
     */
     return new Date().toLocaleDateString('en-US', option);
   }
+}
+
+// Running
+interface RunningProps extends WorkoutProps {
+  cadence: number;
+  pace: number;
+}
+
+export class Running extends Workout {
+  type = 'Running';
+  cadence: number;
+  pace: number;
+
+  constructor(values: RunningProps) {
+    super(values);
+    this.cadence = values.cadence;
+    this.pace = this._calcPace();
+  }
 
   _calcPace() {
     // min/km
-    this.pace = this.duration / this.distance;
-    return this.pace;
+    return this.duration / this.distance;
+  }
+}
+
+// Cycling
+interface CyclingProps extends WorkoutProps {
+  elevationGain: number;
+  speed: number;
+}
+
+export class Cycling extends Workout {
+  type = 'Cycling';
+  elevationGain: number;
+  speed: number;
+
+  constructor(values: CyclingProps) {
+    super(values);
+    this.elevationGain = values.elevationGain;
+    this.speed = this._calcSpeed();
   }
 
   _calcSpeed() {
     // km/h
-    this.speed = this.distance / (this.duration / 60);
-    return this.speed;
+    return (this.distance / this.duration) * 60;
   }
 }
