c3147cdace17ef4d7c8997543caaaf1c2755b27d
Withlaw <sanxang@gmail.com>
2023-12-15 22:26:12 +0900

    forkify: router 생성

--- JS_ZtoE/Forkify/react-app/src/App.tsx
+++ JS_ZtoE/Forkify/react-app/src/App.tsx
@@ -1,10 +1,29 @@
 import React from "react";
+import {
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+
 import "@/sass/main.scss";
 
 import Layout from "@/components/Layout";
 
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <Layout />,
+  },
+];
+
+const router = createBrowserRouter(
+  routes.map(route => {
+    return route;
+  })
+);
+
 function App() {
-  return <Layout />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
