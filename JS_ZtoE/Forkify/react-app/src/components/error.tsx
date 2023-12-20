ca706be1eecc537f69ac429b6016b919c80c73a6
Withlaw <sanxang@gmail.com>
2023-12-21 02:08:24 +0900

    forkify: error component 생성

---
+++ JS_ZtoE/Forkify/react-app/src/components/error.tsx
@@ -0,0 +1,16 @@
+import icons from "@/assets/icons/icons.svg";
+
+const Error = ({ message = "error ouccured" }: { message: string }) => {
+  return (
+    <div className="error">
+      <div>
+        <svg>
+          <use href={`${icons}#icon-alert-triangle`}></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>
+  );
+};
+
+export default Error;
