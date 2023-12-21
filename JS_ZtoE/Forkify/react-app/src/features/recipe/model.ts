578c0af1aa4a5d7d1fb760fdf5eca75dbaf53f5e
Withlaw <sanxang@gmail.com>
2023-12-22 01:47:47 +0900

    forkify: new recipe modal 구현

--- JS_ZtoE/Forkify/react-app/src/features/recipe/model.ts
+++ JS_ZtoE/Forkify/react-app/src/features/recipe/model.ts
@@ -1,4 +1,5 @@
-export type RecipeData = {
+// recipe data preview
+export type RecipeDataPreview = {
   id: string;
   publisher: string;
   title: string;
@@ -19,7 +20,7 @@ export class Recipe implements RecipeI {
   publisher: string;
   title: string;
 
-  constructor(data: RecipeData) {
+  constructor(data: RecipeDataPreview) {
     const { id, publisher, title } = data;
     this.id = id;
     this.publisher = publisher;
@@ -28,32 +29,36 @@ export class Recipe implements RecipeI {
   }
 }
 
-//
+// recipe data
 export type IngredientType = {
   quantity?: number;
   unit: string;
   description: string;
 };
 
-export type RecipeDetailData = {
-  id: string;
-  publisher: string;
-  title: string;
-  servings: number;
-  ingredients: IngredientType[];
-  source_url?: string;
-  sourceUrl?: string;
-  image_url?: string;
-  imageUrl?: string;
-  cooking_time?: number;
-  cookingTime?: number;
-};
+export interface RecipeData {
+  readonly id?: string;
+  readonly publisher: string;
+  readonly title: string;
+  readonly servings: number;
+  readonly source_url?: string;
+  readonly sourceUrl?: string;
+  readonly image_url?: string;
+  readonly imageUrl?: string;
+  readonly cooking_time?: number;
+  readonly cookingTime?: number;
+}
+
+export interface RecipeDetailData extends RecipeData {
+  readonly ingredients: IngredientType[];
+}
+
 export type RecipeDetailDataRes = {
   recipe: RecipeDetailData;
 };
 
 export interface RecipeDetailI {
-  readonly id: string;
+  readonly id?: string;
   readonly publisher: string;
   readonly title: string;
   readonly servings: number;
@@ -64,7 +69,7 @@ export interface RecipeDetailI {
 }
 
 export class RecipeDetail implements RecipeDetailI {
-  id: string;
+  id?: string;
   publisher: string;
   title: string;
   servings: number;
@@ -81,5 +86,64 @@ export class RecipeDetail implements RecipeDetailI {
     this.sourceUrl = data.sourceUrl || data.source_url || "";
     this.imageUrl = data.imageUrl || data.image_url || "";
     this.cookingTime = data.cookingTime || data.cooking_time || 0;
+  }
+}
+
+// new recipe format
+export class RecipeFormData implements RecipeDetailData {
+  id?: string;
+  publisher: string;
+  title: string;
+  servings: number;
+  ingredients: IngredientType[];
+  source_url: string;
+  image_url: string;
+  cooking_time: number;
+  constructor(data: RecipeDetailI) {
+    this.id = data.id;
+    this.publisher = data.publisher;
+    this.title = data.title;
+    this.servings = data.servings;
+    this.ingredients = data.ingredients;
+    this.source_url = data.sourceUrl;
+    this.image_url = data.imageUrl;
+    this.cooking_time = data.cookingTime;
+  }
+
+  static extractIngredients(data: {
+    [name: string]: string;
+  }): IngredientType[] {
+    return Object.entries(data)
+      .filter(el => el[0].startsWith("ingredient") && el[1] !== "")
+      .map(el => {
+        const ingredientsOnly = el[1].split(",").map(el => el.trim());
+
+        if (ingredientsOnly.length !== 3)
+          throw new Error("Wrong ingredient format!");
+
+        const [quantity, unit, description] = ingredientsOnly;
+
+        return {
+          quantity: quantity ? +quantity : undefined,
+          unit,
+          description,
+        };
+      });
+  }
+  static extractRecipeData(data: { [name: string]: string }): RecipeData {
+    // const recipeDataOnlyArr = Object.entries(data).filter(
+    //   el => !el[0].startsWith("ingredient")
+    // );
+
+    // return Object.fromEntries(recipeDataOnlyArr);
+
+    return {
+      title: data.title,
+      source_url: data.sourceUrl,
+      image_url: data.image,
+      publisher: data.publisher,
+      cooking_time: +data.cookingTime,
+      servings: +data.servings,
+    };
   }
 }
